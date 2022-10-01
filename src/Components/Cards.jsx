8273a81efc45cb07e4d5b238239facbf0087a578
kerley pablo <kerley.pablo@hotmail.com>
2022-10-01 18:41:09 -0300

    correcao lint

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -1,17 +1,17 @@
-import React from "react";
+import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import { Link } from 'react-router-dom'
-import './Styles/Cards.css'
+import { Link } from 'react-router-dom';
+import './Styles/Cards.css';
 
-function Cards({ textifo ='text', img=''}) {
+function Cards({ textifo = 'text', img = '' }) {
   return (
     <Card className="card" style={{ width: '180px' }}>
-      <Card.Img className="cardImg" variant="top" src={ img } />
+      <Card.Img className="cardImg" variant="top" src={img} />
       <Card.Body className="cardBody">
         <Card.Text className="cardTitle">{ textifo }</Card.Text>
         <Link to="/logar">
-        <Button variant="primary">saiba mais</Button>
+          <Button variant="primary">saiba mais</Button>
         </Link>
       </Card.Body>
     </Card>
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,26 +1,24 @@
-import React from "react";
-import Cards from "./Cards";
-import './Styles/footer.css'
-import logo from '../images/doces-em-goiania-lugares-para-provar-verdadeiras-delicias-9.jpg'
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
-
+import Cards from './Cards';
+import './Styles/footer.css';
+import logo from '../images/doces-em-goiania-lugares-para-provar-verdadeiras-delicias-9.jpg';
+import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
 
 export default function Footer() {
-
-  return(
+  return (
     <footer>
-      <div className="simbolo"></div>
+      <div className="simbolo" />
       <div className="roleta">
-    <Carousel className="carosel" centerMode width={950} centerSlidePercentage={ 20 } infiniteLoop >
-      <Cards texto= 'teste' img={ logo } />
-      <Cards texto= 'teste' img={ logo } />
-      <Cards texto= 'teste' img={ logo } />
-      <Cards texto= 'teste' img={ logo } />
-      <Cards texto= 'teste' img={ logo } />
-      <Cards texto= 'teste' img={ logo } />
-    </Carousel>
+        <Carousel className="carosel" centerMode width={950} centerSlidePercentage={20} infiniteLoop>
+          <Cards texto="teste" img={logo} />
+          <Cards texto="teste" img={logo} />
+          <Cards texto="teste" img={logo} />
+          <Cards texto="teste" img={logo} />
+          <Cards texto="teste" img={logo} />
+          <Cards texto="teste" img={logo} />
+        </Carousel>
       </div>
     </footer>
-  )
+  );
 }
